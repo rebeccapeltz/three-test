7ef33459707451f4045db8246846e15fac4ce60f
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-05-31 14:45:29 -0700

    modify crop mode;turn on animation;add personalize page;update readme

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # Testing THREE.js
+THREE uses modules.  Parcel is used for building.
+I'm copying the code I need in to source.  
+NPM install is just needed for parcel.
 
+This is deployed on netlify: https://bright-mousse-6903a7.netlify.app/
 
+## load parcel
+`nmp i` or `yarn install`
+
+## run dev
+`yarn start`
+
+## run build
+`yarn build`
+
+## Learn how to download
  [Download](https://www.youtube.com/watch?v=tD0NYabdf5Q)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 import { GLTFExporter } from "./jsm/exporters/GLTFExporter.js";
 import { OrbitControls } from './jsm/controls/OrbitControls.js';
-const URL1 = "https://res.cloudinary.com/picturecloud7/image/upload/h_1024,w_1024,c_limit,f_auto,q_auto:best/saddle-rock";
-const URL2 = "https://res.cloudinary.com/picturecloud7/image/upload/h_1024,w_1024,c_limit,f_auto,q_auto:best/sr-2-kids";
-const URL3 = "https://res.cloudinary.com/picturecloud7/image/upload/w_1024,h_1024,c_limit,f_auto,q_auto:best/jim-mission-ridge";
+const URL1 = "https://res.cloudinary.com/picturecloud7/image/upload/h_1024,w_1024,c_fill,g_auto,f_auto,q_auto:best/saddle-rock";
+const URL2 = "https://res.cloudinary.com/picturecloud7/image/upload/h_1024,w_1024,c_fill,g_auto,f_auto,q_auto:best/sr-2-kids";
+const URL3 = "https://res.cloudinary.com/picturecloud7/image/upload/w_1024,h_1024,c_fill,g_auto,f_auto,q_auto:best/jim-mission-ridge";
 
 let container;
 let camera;
@@ -57,7 +57,7 @@ function createMeshes() {
     ].map(pic => {
         let texture = textureLoader.load(pic)
         texture.encoding = THREE.sRGBEncoding;
-        debugger
+        // debugger
         return new THREE.MeshLambertMaterial({ map: texture });
         return new THREE.MeshLambertMaterial({map: textureLoader.load(pic).encoding=THREE.sRGBEncoding});
     });
@@ -84,9 +84,9 @@ function createControls() {
 }
 
 function update() {
-//     mesh.rotation.x += 0.01;
-//     mesh.rotation.y += 0.01;
-//     mesh.rotation.z += 0.01;
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.01;
 }
 
 function render() {
