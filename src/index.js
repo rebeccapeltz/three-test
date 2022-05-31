8bf909a58c3f148f24cdb39391841c6561d9419d
rebeccapeltz <rebeccapeltz@gmail.com>
2022-05-30 17:25:08 -0700

    add fauto,qauto

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ function createLights() {
 function createMeshes() {
     const textureLoader = new THREE.TextureLoader();
     // debugger
-    const texture = textureLoader.load("https://res.cloudinary.com/picturecloud7/image/upload/h_1024,w_1024,c_limit/saddle-rock-columbia-river_vzx2br.avif");
+    const texture = textureLoader.load("https://res.cloudinary.com/picturecloud7/image/upload/h_1024,w_1024,c_limit,f_auto,q_auto:best/saddle-rock-columbia-river_vzx2br");
     texture.encoding = THREE.sRGBEncoding;
 
     const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
